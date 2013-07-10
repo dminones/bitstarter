d069d9760ba958a8b1f6199cca6c24ce1b03b291
Ubuntu <ubuntu@domU-12-31-39-0F-8E-D5.compute-1.internal>
2013-07-10 05:42:41 +0000

    agrego fs

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+    var fs = require('fs');
+    
     fs.readFile('index.html', function (err, data) {
 	if (err) throw err;
 	response.send(data.toString());
